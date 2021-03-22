e3160c3c54c2af7e16ca6e0e6d2d05c7ddd6d4ca
mister_person <mister.person@icloud.com>
2021-03-22 03:11:16 -0700

    initial commit lol

---
+++ README.md
@@ -0,0 +1,5 @@
+haha I don't have to get up to control my desktop. yay ssh
+why have I done this
+I should go to bed
+
+alt is command, ctrl-c to quit, hacks for cmd-tab and switch space
---
+++ src/main.rs
@@ -0,0 +1,85 @@
+use termion::input::TermRead;
+use termion::event::Event;
+use std::io::{Write, stdout, stdin};
+use termion::raw::IntoRawMode;
+use termion::cursor::DetectCursorPos;
+use enigo::Enigo;
+use enigo::KeyboardControllable;
+
+fn main() {
+    let mut stdout = stdout().into_raw_mode().unwrap();
+    let stdin = stdin();
+    let mut stdin = stdin.lock();
+
+    let mut enigo = Enigo::new();
+
+    for e in stdin.events() {
+        let e = e.unwrap();
+        match e {
+            Event::Key(key) => {
+                let key = key;
+                let pos = stdout.cursor_pos().unwrap();
+                write!(stdout, "{:?}{}\n", key, termion::cursor::Goto(1, pos.1)).unwrap();
+                match key {
+                    termion::event::Key::Backspace => {enigo.key_click(enigo::Key::Backspace)}
+                    termion::event::Key::Left => {enigo.key_click(enigo::Key::LeftArrow)}
+                    termion::event::Key::Right => {enigo.key_click(enigo::Key::RightArrow)}
+                    termion::event::Key::Up => {enigo.key_click(enigo::Key::UpArrow)}
+                    termion::event::Key::Down => {enigo.key_click(enigo::Key::DownArrow)}
+                    termion::event::Key::Home => {}
+                    termion::event::Key::End => {}
+                    termion::event::Key::PageUp => {}
+                    termion::event::Key::PageDown => {}
+                    termion::event::Key::BackTab => {}
+                    termion::event::Key::Delete => {}
+                    termion::event::Key::Insert => {}
+                    termion::event::Key::F(_) => {}
+                    termion::event::Key::Char('\n') => {
+                        enigo.key_click(enigo::Key::Return);
+                        enigo.key_up(enigo::Key::Meta);
+                    }
+                    termion::event::Key::Char(key) => {
+                        if key.is_uppercase() {
+                            enigo.key_down(enigo::Key::Shift);
+                            enigo.key_click(enigo::Key::Layout(key));
+                            enigo.key_up(enigo::Key::Shift);
+                        }
+                        else {
+                            enigo.key_click(enigo::Key::Layout(key));
+                        }
+                    }
+                    termion::event::Key::Alt('{') => {
+                        enigo.key_down(enigo::Key::Control);
+                        enigo.key_click(enigo::Key::LeftArrow);
+                        enigo.key_up(enigo::Key::Control);
+                    }
+                    termion::event::Key::Alt('}') => {
+                        enigo.key_down(enigo::Key::Control);
+                        enigo.key_click(enigo::Key::RightArrow);
+                        enigo.key_up(enigo::Key::Control);
+                    }
+                    termion::event::Key::Alt('\t') => {
+                        enigo.key_down(enigo::Key::Meta);
+                        enigo.key_click(enigo::Key::Tab);
+                    }
+                    termion::event::Key::Alt(key) => {
+                        enigo.key_down(enigo::Key::Meta);
+                        enigo.key_click(enigo::Key::Layout(key));
+                        enigo.key_up(enigo::Key::Meta);
+                    }
+                    termion::event::Key::Ctrl('c') => {panic!()}
+                    termion::event::Key::Ctrl(key) => {
+                        enigo.key_down(enigo::Key::Control);
+                        enigo.key_click(enigo::Key::Layout(key));
+                        enigo.key_up(enigo::Key::Control);
+                    }
+                    termion::event::Key::Null => {}
+                    termion::event::Key::Esc => {enigo.key_click(enigo::Key::Escape)}
+                    termion::event::Key::__IsNotComplete => {}
+                }
+            }
+            Event::Mouse(_) => {}
+            _ => {}
+        }
+    }
+}
