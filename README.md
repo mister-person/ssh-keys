fbdd703326f58eac93ddb90a35381f6b4afa4c9a
mister_person <mister.person@icloud.com>
2021-05-24 17:45:41 -0700

    made readme less silly and changed keybinds for new setup

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-haha I don't have to get up to control my desktop. yay ssh
-why have I done this
-I should go to bed
+Have you ever wanted to use your desktop computer but it's all the way across the room? Well this is the program for you. Just ssh into it, run this program with `cargo run`, and it will turn inputs from ssh into key presses on the computer running it.
 
-alt is command, ctrl-c to quit, hacks for cmd-tab and switch space
+lol
+
+Modifier keys are remapped, ctrl-c twice to quit, hacks for cmd-tab and switch space
--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,12 @@ use enigo::KeyboardControllable;
 fn main() {
     let mut stdout = stdout().into_raw_mode().unwrap();
     let stdin = stdin();
-    let mut stdin = stdin.lock();
+    let stdin = stdin.lock();
 
     let mut enigo = Enigo::new();
+
+    let mut alt_tabbing = false;
+    let mut escaping = false;
 
     for e in stdin.events() {
         let e = e.unwrap();
@@ -20,6 +23,15 @@ fn main() {
                 let key = key;
                 let pos = stdout.cursor_pos().unwrap();
                 write!(stdout, "{:?}{}\n", key, termion::cursor::Goto(1, pos.1)).unwrap();
+                if let termion::event::Key::Ctrl('c') = key {
+                    if escaping {
+                        break
+                    }
+                    escaping = true;
+                }
+                else {
+                    escaping = false;
+                }
                 match key {
                     termion::event::Key::Backspace => {enigo.key_click(enigo::Key::Backspace)}
                     termion::event::Key::Left => {enigo.key_click(enigo::Key::LeftArrow)}
@@ -30,13 +42,25 @@ fn main() {
                     termion::event::Key::End => {}
                     termion::event::Key::PageUp => {}
                     termion::event::Key::PageDown => {}
-                    termion::event::Key::BackTab => {}
+                    termion::event::Key::BackTab => {
+                        enigo.key_down(enigo::Key::Shift);
+                        enigo.key_click(enigo::Key::Tab);
+                        enigo.key_up(enigo::Key::Shift);
+                    }
+                    termion::event::Key::Char('\t') => {
+                        enigo.key_click(enigo::Key::Tab);
+                    }
                     termion::event::Key::Delete => {}
                     termion::event::Key::Insert => {}
                     termion::event::Key::F(_) => {}
                     termion::event::Key::Char('\n') => {
-                        enigo.key_click(enigo::Key::Return);
-                        enigo.key_up(enigo::Key::Meta);
+                        if alt_tabbing {
+                            enigo.key_up(enigo::Key::Control);
+                            alt_tabbing = false;
+                        }
+                        else {
+                            enigo.key_click(enigo::Key::Return);
+                        }
                     }
                     termion::event::Key::Char(key) => {
                         if key.is_uppercase() {
@@ -49,29 +73,31 @@ fn main() {
                         }
                     }
                     termion::event::Key::Alt('{') => {
-                        enigo.key_down(enigo::Key::Control);
+                        enigo.key_down(enigo::Key::Meta);
                         enigo.key_click(enigo::Key::LeftArrow);
-                        enigo.key_up(enigo::Key::Control);
+                        enigo.key_up(enigo::Key::Meta);
                     }
                     termion::event::Key::Alt('}') => {
-                        enigo.key_down(enigo::Key::Control);
+                        enigo.key_down(enigo::Key::Meta);
                         enigo.key_click(enigo::Key::RightArrow);
-                        enigo.key_up(enigo::Key::Control);
+                        enigo.key_up(enigo::Key::Meta);
                     }
                     termion::event::Key::Alt('\t') => {
-                        enigo.key_down(enigo::Key::Meta);
+                        if !alt_tabbing {
+                            enigo.key_down(enigo::Key::Control);
+                        }
                         enigo.key_click(enigo::Key::Tab);
+                        alt_tabbing = true;
                     }
                     termion::event::Key::Alt(key) => {
+                        enigo.key_down(enigo::Key::Control);
+                        enigo.key_click(enigo::Key::Layout(key));
+                        enigo.key_up(enigo::Key::Control);
+                    }
+                    termion::event::Key::Ctrl(key) => {
                         enigo.key_down(enigo::Key::Meta);
                         enigo.key_click(enigo::Key::Layout(key));
                         enigo.key_up(enigo::Key::Meta);
-                    }
-                    termion::event::Key::Ctrl('c') => {panic!()}
-                    termion::event::Key::Ctrl(key) => {
-                        enigo.key_down(enigo::Key::Control);
-                        enigo.key_click(enigo::Key::Layout(key));
-                        enigo.key_up(enigo::Key::Control);
                     }
                     termion::event::Key::Null => {}
                     termion::event::Key::Esc => {enigo.key_click(enigo::Key::Escape)}
@@ -81,5 +107,8 @@ fn main() {
             Event::Mouse(_) => {}
             _ => {}
         }
+    }
+    if alt_tabbing {
+        enigo.key_up(enigo::Key::Control);
     }
 }
